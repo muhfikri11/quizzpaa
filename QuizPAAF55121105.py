df6d5a11a6a1c4dd4595f00173092b1224d68c68
muhfikri11 <88622753+muhfikri11@users.noreply.github.com>
2022-12-13 21:01:01 +0800

    Add files via upload

---
+++ QuizPAAF55121105.py
@@ -0,0 +1,14 @@
+# Muh. Fikri Firman
+# F55121105
+
+muhfikri = []
+y = 3
+while y < 20:
+    if y % 3 == 0:
+        muhfikri.append(y)
+    y = y + 3
+print (muhfikri)
+fikri = 0
+for i in muhfikri:
+    fikri = fikri + i
+print (fikri)
